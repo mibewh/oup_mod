03dfcba517f66a6d8ecde3eb8f13b06f00b6f5e8
padgettm <mick.padgett@oup.com>
2018-03-22 13:03:38 +0000

    initial creation from msample module

---
+++ module/oup-ui/index.js
@@ -0,0 +1,9 @@
+define(function(require) {
+
+    // action: "create-journal"
+    require("./actions/create-journal/create-journal.js");
+    
+    // use this to globally control the position of helper text
+    Alpaca.defaultHelpersPosition = "above";
+    
+});
