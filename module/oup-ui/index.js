b3922205befeb4302e461995f05a1ff207e4b465
padgettm <mick.padgett@oup.com>
2018-03-29 11:42:57 +0100

    adding edit secondary page action

--- module/oup-ui/index.js
+++ module/oup-ui/index.js
@@ -2,6 +2,7 @@ define(function(require) {
 
     // action: "create-journal"
     //require("./actions/create-journal/create-journal.js");
+    require("./actions/create-journal/edit-secondaryPage.js");
     require("./actions/create-journal/create-secondaryPage.js");
     
     // use this to globally control the position of helper text
