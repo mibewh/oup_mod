4e761ea4a105f15a8476c18c87d658ec302099f3
padgettm <mick.padgett@oup.com>
2018-04-20 14:26:31 +0100

    updated from Gitanas latest release - add screen overrides

--- module/oup-ui/index.js
+++ module/oup-ui/index.js
@@ -1,11 +1,18 @@
 define(function(require) {
 
     // action: "create-journal"
-    //require("./actions/create-journal/create-journal.js");
-    require("./actions/create-journal/edit-secondaryPage.js");
     require("./actions/create-journal/create-secondaryPage.js");
     
-    // use this to globally control the position of helper text
-    Alpaca.defaultHelpersPosition = "above";
+    // action: "edit-journal"
+    require("./actions/edit-journal/edit-secondaryPage.js");
     
+    // action: "open-oup"
+    //require("./actions/open-oup/open-oup.js");
+
+    // new pages
+    require("./gadgets/all-journals-list.js");
+    require("./gadgets/secondary-pages-list.js");
+    
+    // use this to globally control the position of helper text
+    Alpaca.defaultHelpersPosition = "above";    
 });
