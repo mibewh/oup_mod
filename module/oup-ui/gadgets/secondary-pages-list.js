aa3d5d0869ce0363a28fa89a3a0dc7a337b40591
padgettm <mick.padgett@oup.com>
2018-04-20 14:35:40 +0100

    updated

--- module/oup-ui/gadgets/secondary-pages-list.js
+++ module/oup-ui/gadgets/secondary-pages-list.js
@@ -2,7 +2,7 @@
 define(function(require, exports, module) {
 
     var Ratchet = require("ratchet/web");
-    var DocumentsList = require("/oneteam/modules/app/gadgets/project/documents/documents-list.js");
+    var DocumentsList = require("/oneteam-1523937005406/modules/app/gadgets/project/documents/documents-list.js");
     var OneTeam = require("oneteam");
 
     return Ratchet.GadgetRegistry.register("secondary-pages-list", DocumentsList.extend({
