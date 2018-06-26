3f7acac6ceab762eaf74ce8a00b8b43fb75b68f4
palak-parikh-oup <palak.parikh@oup.com>
2018-06-26 12:07:01 +0100

    udpate

--- module/oup-ui/gadgets/all-journals-list.js
+++ module/oup-ui/gadgets/all-journals-list.js
@@ -119,7 +119,7 @@ define(function(require, exports, module) {
                 value += "<a href='#/projects/" + project._doc + "/documents/" + row._doc + "/browse'>";
                 value += row.siteSortname;
                 value += "</a>";
-                return value;
+                return row.siteSortname;
             }
 
             if (item.key === "journalCode") {
