cced0d6cd073e3fe8bab50451b37a18b99e10917
mwhitman <mwhitman95@gmail.com>
2021-11-05 13:05:54 -0400

    only do custom journal site folder logic in "this folder" mode

--- module/oup-ui/gadgets/journal-site-folder.js
+++ module/oup-ui/gadgets/journal-site-folder.js
@@ -38,137 +38,145 @@ define(function(require, exports, module) {
         {
             var self = this;
 
-            if (OneTeam.isEmptyOrNonExistent(query) && searchTerm)
+            if (this.searchState() === "children")
             {
-                query = OneTeam.searchQuery(searchTerm, ["title", "description"]);
-            }
 
-            if (!query)
-            {
-                query = {};
-            }
-            var config = {
-                "type": "a:child",
-                "direction": "OUTGOING"
-            };
-
-            var settings = self.observable("oneteamUserSettings").get();
-            if (settings)
-            {
-                var hideSystemFolders = settings["hideSystemFolders"];
-                if (hideSystemFolders)
+                if (OneTeam.isEmptyOrNonExistent(query) && searchTerm)
                 {
-                    // filter out any nodes that are tagged as system folders
-                    query = {
-                        "$and": [query, {
-                            "_features.f:system-folder.enabled": {
-                                "$ne": true
-                            }
-                        }]
-                    };
-
-                    // if we're at root node, also filter out "System" and "Templates" and "Definitions" folders
-                    var rootNode = self.observable("rootNode").get();
-                    if (rootNode && rootNode.getId() === node.getId())
+                    query = OneTeam.searchQuery(searchTerm, ["title", "description"]);
+                }
+    
+                if (!query)
+                {
+                    query = {};
+                }
+                var config = {
+                    "type": "a:child",
+                    "direction": "OUTGOING"
+                };
+    
+                var settings = self.observable("oneteamUserSettings").get();
+                if (settings)
+                {
+                    var hideSystemFolders = settings["hideSystemFolders"];
+                    if (hideSystemFolders)
                     {
-                        query["$and"].push({
-                            "title": {
-                                "$ne": "System"
-                            }
-                        });
-                        query["$and"].push({
-                            "title": {
-                                "$ne": "Templates"
-                            }
-                        });
-                        query["$and"].push({
-                            "title": {
-                                "$ne": "Definitions"
-                            }
-                        });
+                        // filter out any nodes that are tagged as system folders
+                        query = {
+                            "$and": [query, {
+                                "_features.f:system-folder.enabled": {
+                                    "$ne": true
+                                }
+                            }]
+                        };
+    
+                        // if we're at root node, also filter out "System" and "Templates" and "Definitions" folders
+                        var rootNode = self.observable("rootNode").get();
+                        if (rootNode && rootNode.getId() === node.getId())
+                        {
+                            query["$and"].push({
+                                "title": {
+                                    "$ne": "System"
+                                }
+                            });
+                            query["$and"].push({
+                                "title": {
+                                    "$ne": "Templates"
+                                }
+                            });
+                            query["$and"].push({
+                                "title": {
+                                    "$ne": "Definitions"
+                                }
+                            });
+                        }
                     }
                 }
-            }
-
-            var node = model.node;
-
-            var homePageList = [];
-            var secondaryPageList = [];
-            var containerItemsList = [];
-            var imageLibraryList = [];
-            var documentLibraryList = [];
-
-            self.base(context, model, searchTerm, query, pagination, function(queryResult) {
-                Chain(queryResult).then(function() {
-
-                    this.each(function() {
-
-                        if(this.getTypeQName() == "type:journalhomepage0")
-                        {
-                            homePageList.push(this);
-                        }
-                        if (this.getTypeQName() == "type:secondarypagesfolder0")
-                        {
-                            secondaryPageList.push(this);
-                        }
-                        if (this.getTypeQName() == "type:containeritemsfolder0")
-                        {
-                            containerItemsList.push(this);
-                        }
-                        if (this.getTypeQName() == "type:imagelibraryfolder0")
-                        {
-                            imageLibraryList.push(this);
-                        }
-                        if (this.getTypeQName() == "type:documentlibraryfolder0")
-                        {
-                            documentLibraryList.push(this);
-                        }
-        
-                    }).then(function() {
-                        var list =[];
-        
-                        var result = {
-                            "rows": []
-                        };
-        
-                        if(homePageList.length > 0)
-                        {
-                            list.push(homePageList);
-                        }
-        
-                        if(secondaryPageList.length > 0)
-                        {
-                            list.push(secondaryPageList);
-                        }
-        
-                        if(containerItemsList.length > 0)
-                        {
-                            list.push(containerItemsList);
-                        }
-        
-                        if(imageLibraryList.length > 0)
-                        {
-                            list.push(imageLibraryList);
-                        }
-        
-                        if(documentLibraryList.length > 0)
-                        {
-                            list.push(documentLibraryList);
-                        }
-        
-                        for( var i =0; i <list.length; i++)
-                        {
-                            result.rows.push(list[i][0]);
-                        }
-
-                        var branch = node.getBranch();
-                        var map = branch.getFactory().nodeMap(branch, {});
-                        map.handleResponse(result);
-
-                        callback(map);
+    
+                var node = model.node;
+    
+                var homePageList = [];
+                var secondaryPageList = [];
+                var containerItemsList = [];
+                var imageLibraryList = [];
+                var documentLibraryList = [];
+    
+                self.base(context, model, searchTerm, query, pagination, function(queryResult) {
+                    Chain(queryResult).then(function() {
+    
+                        this.each(function() {
+    
+                            if(this.getTypeQName() == "type:journalhomepage0")
+                            {
+                                homePageList.push(this);
+                            }
+                            if (this.getTypeQName() == "type:secondarypagesfolder0")
+                            {
+                                secondaryPageList.push(this);
+                            }
+                            if (this.getTypeQName() == "type:containeritemsfolder0")
+                            {
+                                containerItemsList.push(this);
+                            }
+                            if (this.getTypeQName() == "type:imagelibraryfolder0")
+                            {
+                                imageLibraryList.push(this);
+                            }
+                            if (this.getTypeQName() == "type:documentlibraryfolder0")
+                            {
+                                documentLibraryList.push(this);
+                            }
+            
+                        }).then(function() {
+                            var list =[];
+            
+                            var result = {
+                                "rows": []
+                            };
+            
+                            if(homePageList.length > 0)
+                            {
+                                list.push(homePageList);
+                            }
+            
+                            if(secondaryPageList.length > 0)
+                            {
+                                list.push(secondaryPageList);
+                            }
+            
+                            if(containerItemsList.length > 0)
+                            {
+                                list.push(containerItemsList);
+                            }
+            
+                            if(imageLibraryList.length > 0)
+                            {
+                                list.push(imageLibraryList);
+                            }
+            
+                            if(documentLibraryList.length > 0)
+                            {
+                                list.push(documentLibraryList);
+                            }
+            
+                            for( var i =0; i <list.length; i++)
+                            {
+                                result.rows.push(list[i][0]);
+                            }
+    
+                            var branch = node.getBranch();
+                            var map = branch.getFactory().nodeMap(branch, {});
+                            map.handleResponse(result);
+    
+                            callback(map);
+                        });
                     });
                 });
-            });
+            }
+            else
+            {
+                this.base(context, model, searchTerm, query, pagination, callback);
+            }
         },
 
         columnValue: function(row, item, model, context)
